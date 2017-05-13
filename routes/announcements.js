43ac65f9c5eed5f2bda29a199c3792c3433702c8
sghaida <saddam.abughaida@gmail.com>
2017-05-13 19:51:41 +0300

    add view for create announcements and its related route

--- routes/announcements.js
+++ routes/announcements.js
@@ -10,8 +10,29 @@ var express         = require('express'),
 
 
 router.get('/',auth.ensureLoggedIn('/login'), function (req,res, next) {
-    res.render('announcements');
+    res.render('announcements/view');
 });
+
+router.post('/', auth.ensureLoggedIn('/login'), function (req, res, next) {
+    Announcement.create(req.body.announcement, function (err, announcement) {
+        if(err){
+            console.log(err);
+        } else {
+            announcement.author.id = req.user._id;
+            announcement.author.name = req.user.name;
+            req.body.announcement.tags.forEach(function(tag){
+                announcement.tags.push(tag);
+            });
+            announcement.save();
+        }
+    });
+})
+
+router.get('/new', auth.ensureLoggedIn('/login'), function (req,res, next) {
+    res.render('announcements/new');
+});
+
+
 
 module.exports = router;
 
