ca1c8388644707486eb51aba1028353a1a43a023
sghaida <saddam.abughaida@gmail.com>
2017-05-13 20:39:32 +0300

    add departments and tags to dropdownlists and handle them

--- routes/announcements.js
+++ routes/announcements.js
@@ -6,7 +6,10 @@ var express         = require('express'),
     router          = express.Router(),
     passport        = require('passport'),
     auth            = require('connect-ensure-login'),
-    Announcement    = require('../models/announcement');
+    Announcement    = require('../models/announcement'),
+    Department      = require('../models/department'),
+    Tag             = require('../models/tag'),
+    async           = require('async');
 
 
 router.get('/',auth.ensureLoggedIn('/login'), function (req,res, next) {
@@ -23,13 +26,32 @@ router.post('/', auth.ensureLoggedIn('/login'), function (req, res, next) {
             req.body.announcement.tags.forEach(function(tag){
                 announcement.tags.push(tag);
             });
+            announcement.department = req.body.announcement.department
             announcement.save();
+
+            res.render('/announcements/view');
         }
     });
 })
 
 router.get('/new', auth.ensureLoggedIn('/login'), function (req,res, next) {
-    res.render('announcements/new');
+   async.parallel([
+           function (cb) {
+               Tag.find({},cb);
+           },
+           function (cb) {
+               Department.find({},cb);
+           }
+       ],function (err, result) {
+            if(err) {
+                console.log('fuckin error' + err)
+            }else {
+                console.log(result);
+                console.log('fuck');
+                res.render('announcements/new', {tags: result[0], departments: result[1]});
+            }
+   });
+
 });
 
 
