df414e284c402a6c29e7588439f706594f35bf4f
sghaida <saddam.abughaida@gmail.com>
2017-05-13 20:40:01 +0300

    add testing route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,20 @@ var express     = require('express'),
     router      = express.Router(),
     passport    = require('passport'),
     auth        = require('connect-ensure-login');
-
 /* GET home page. */
 router.get('/', auth.ensureLoggedIn('/login'), function(req, res, next) {
   console.log(req.currentUser);
   res.render('index');
+});
+
+router.get('/test',auth.ensureLoggedIn('/login'), function(req, res, next) {
+    console.log(req.currentUser);
+    res.render('test');
+});
+
+router.post('/test', function (req, res, next) {
+    console.log('tags is : ' + req.body.tags);
+
 });
 
 module.exports = router;
