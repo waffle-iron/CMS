3d276e374e8b04b6dfea92884d9ea43dc91d3561
Robert Naccache <RNaccache@ccc.net>
2017-05-15 19:08:29 +0300

    Added Announcement form

---
+++ public/javascripts/config.js
@@ -0,0 +1,22 @@
+/**
+ * Created by RNaccache on 5/15/2017.
+ * For ckEditor
+ */
+CKEDITOR.editorConfig = function( config ) {
+    config.toolbarGroups = [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+        { name: 'links', groups: [ 'links' ] },
+        { name: 'insert', groups: [ 'insert' ] },
+        { name: 'forms', groups: [ 'forms' ] },
+        { name: 'tools', groups: [ 'tools' ] },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'others', groups: [ 'others' ] },
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+        { name: 'styles', groups: [ 'styles' ] },
+        { name: 'colors', groups: [ 'colors' ] }
+    ];
+
+    config.removeButtons = 'Underline,Subscript,Superscript,Anchor';
+};
