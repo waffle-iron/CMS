d49d0f9fc6c561adc80cca34bb175b0c233b11c4
sghaida <saddam.abughaida@gmail.com>
2017-05-19 15:55:53 +0300

    Implement the / route for the index page to list all applications

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 var express     = require('express'),
     router      = express.Router(),
     passport    = require('passport'),
-    auth        = require('connect-ensure-login');
+    auth        = require('connect-ensure-login'),
+    Application = require('../models/application');
+
 /* GET home page. */
 router.get('/', auth.ensureLoggedIn('/login'), function(req, res, next) {
-  console.log(req.currentUser);
-  res.render('index');
+    Application.find({}, function (err, apps) {
+       if(err){
+           console.log(err);
+       } else {
+           res.render('index', {applications: apps});
+       }
+    });
+
 });
 
 router.get('/test',auth.ensureLoggedIn('/login'), function(req, res, next) {
