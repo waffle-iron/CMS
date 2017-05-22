8aa8048ea55969d9e169b0901cc1a6ea759e84c0
sghaida <saddam.abughaida@gmail.com>
2017-05-22 12:06:09 +0300

    add users route for view all

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+var express   = require('express'),
+    router    = express.Router(),
+    mongo     = require('mongoose'),
+    User      = require('../models/user')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', auth.ensureLoggedIn('/login'), middleware.isSystemAdmin, function(req, res, next) {
+  User.find({}).populate('roles').exec(function (users,err) {
+     if(err){
+       console.log(err);
+     } else {
+         res.render('users', {users: users});
+     }
+  });
 });
 
 module.exports = router;
