4a93b195ddb0b8ec02b3388356c608720dcbc44d
sghaida <saddam.abughaida@gmail.com>
2017-05-22 12:11:46 +0300

    add related dependencies

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
-var express   = require('express'),
-    router    = express.Router(),
-    mongo     = require('mongoose'),
-    User      = require('../models/user')
+var express     = require('express'),
+    router      = express.Router(),
+    mongo       = require('mongoose'),
+    auth        = require('connect-ensure-login'),
+    middleware  = require('../middleware/authentication'),
+    User        = require('../models/user')
 
 /* GET users listing. */
 router.get('/', auth.ensureLoggedIn('/login'), middleware.isSystemAdmin, function(req, res, next) {
