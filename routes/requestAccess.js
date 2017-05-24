4b48b48d32796f2744f910e00ee68cf5dc193df0
sghaida <saddam.abughaida@gmail.com>
2017-05-24 16:34:24 +0300

    add sendmail capabilities

--- routes/requestAccess.js
+++ routes/requestAccess.js
@@ -9,7 +9,8 @@ var express         = require('express'),
     passport        = require('passport'),
     auth            = require('connect-ensure-login'),
     Application     = require('../models/application'),
-    async           = require('async');
+    async           = require('async'),
+    mailer          = require('nodemailer');
 
 
 router.get('/', auth.ensureLoggedIn('/login'), function (req, res, next) {
@@ -20,6 +21,38 @@ router.get('/', auth.ensureLoggedIn('/login'), function (req, res, next) {
             req.breadcrumbs([{name: 'Applications', url: '/applications'}]);
             res.render('requests/app',{applications: apps, breadcrumbs: req.breadcrumbs()});
         }
+    });
+});
+
+router.post('/', auth.ensureLoggedIn('/login'), function (req, res, next) {
+    var smtpConfig = {
+        host: '10.1.1.128',
+        port: 25,
+        secure: false
+    };
+
+    var transporter = mailer.createTransport(smtpConfig);
+    var mailOptions = {
+        from: req.user.mail,
+        to: 'helpdesk@ccc.net',
+        subject: 'Access reques for : ' + req.body.application,
+        html: 'Dear <strong>helpdesk : <strong>' +
+              '<p></p><p></p>' +
+              '<p>' +
+                  'kinldy grant access for  <strong>' + req.user.mail + '</strong>' +
+                  ' based on the below mentioned reasons: ' +
+                  '<div>' +
+                      req.body.reason +
+                  '</div>' +
+              '</p>'
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+       if(err){
+           console.log(err);
+       } else {
+           console.log('Message %s sent: %s', info.messageId, info.response);
+       }
     });
 });
 
