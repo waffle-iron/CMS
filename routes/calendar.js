ce96dc974fdefec5ca15f889e093e6746b915907
sghaida <saddam.abughaida@gmail.com>
2017-05-24 14:14:32 +0300

    update functionality

--- routes/calendar.js
+++ routes/calendar.js
@@ -8,50 +8,48 @@ var express         = require('express'),
     passport        = require('passport'),
     async           = require('async'),
     http            = require('http');
-    auth            = require('connect-ensure-login');
 
 
-router.get('/:site', auth.ensureLoggedIn('/login'),function (req, res, next) {
+getCalendar = function (site) {
     var data = JSON.stringify({});
     var options = {
-       host: 'orgcal.cf.cloud.ccg.local',
-       port: '80',
-       path: '/cal/orgcal/getby/org/' + req.params.site,
-       method: 'GET',
-       headers: {
-           'Content-Type': 'application/json; charset=utf-8',
-           'Content-Length': data.length
-       }
+        host: 'orgcal.cf.cloud.ccg.local',
+        port: '80',
+        path: '/cal/orgcal/getby/org/' + site,
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json; charset=utf-8',
+            'Content-Length': data.length
+        }
     };
     var req = http.request(options, function (response) {
-       var msg = '';
+        var msg = '';
         response.setEncoding('utf8');
 
         response.on('data', function(chunk) {
-           msg += chunk;
-       });
+            msg += chunk;
+        });
         response.on('end', function() {
-           var calendar = JSON.parse(msg);
-           var holidays = calendar.calendarDetails.holidays
-           var result   = [],
-               counter  = 0;
+            var calendar = JSON.parse(msg);
+            var holidays = calendar.calendarDetails.holidays
+            var result   = [],
+                counter  = 0;
 
             holidays.forEach(function (day) {
-               if(new Date(day.date) >= Date.now() && counter < 5){
-                   counter +=1;
-                   result.push({
-                       date: new Date(day.date),
-                       reason:  day.reason
-                   });
-               }
+                if(new Date(day.date) >= Date.now() && counter < 5){
+                    counter +=1;
+                    result.push({
+                        date: new Date(day.date),
+                        reason:  day.reason
+                    });
+                }
             });
-
-           res.send(result);
-       });
+            return result;
+        });
     });
-
     req.write(data);
     req.end();
-});
+}
 
-module.exports = router;
+
+module.exports = getCalendar;
